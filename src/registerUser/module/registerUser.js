bc5fde3f87dbea4f8301ea64a72e2f8b9fed10da
Vignesh R <68288539+vignesh2398@users.noreply.github.com>
2026-05-18 23:22:37 +0530

    create User changes (#1)

--- src/registerUser/module/registerUser.js
+++ src/registerUser/module/registerUser.js
@@ -7,16 +7,18 @@ export const ValidateUser = async (code) => {
     const userDetail = await userDetails(token.access_token);
     console.log("Access token generated:", token);
     console.log("User details fetched:", userDetail);
-    const existingUser = await User.findOne({ UserId: userDetail.id });
+    const existingUser = await User.findOne({ userid: userDetail.user_id });
     if (existingUser) {
       console.log("User already exists:", existingUser);
       return "User already validated!";
     }
    const user = await User.create({
-    UserId: userDetail.id,
+    id: userDetail.id,
+    userid: userDetail.user_id,
+    username: userDetail.username,
+    account_type: userDetail.account_type,
     accessToken: token.access_token,
     expiresIn: token.expires_in,
-    accountId: token.accountId
    })
    console.log("User created:", user);
     return "User validated successfully!";
