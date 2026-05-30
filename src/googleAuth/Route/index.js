98dd2083315edecb5c8cdeef14ae396244e62402
vignesh2398 <rvignesh60474@gmail.com>
2026-05-30 14:08:33 +0530

    coookie secure

--- src/googleAuth/Route/index.js
+++ src/googleAuth/Route/index.js
@@ -78,7 +78,7 @@ outhrouter.get("/google/callback", async (req, res, next) => {
 
     const cookieOptions = {
       httpOnly: true,
-      secure: false, // Set to true in production with HTTPS
+      secure: true, // Set to true in production with HTTPS
       sameSite: "none",
       maxAge: tokens.expires_in ? tokens.expires_in * 1000 : 24 * 60 * 60 * 1000,
 
